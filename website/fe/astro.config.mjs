5219c4ab85f022baaa902e459af4feeba587e043
Aleksandr Tsertkov <tsertkov@gmail.com>
2024-05-23 10:52:00 +0200

    feat: Add more details on landing page
    - Run xhr request to statusz endpoint
    - Render js and server side vars in lists
    - Add ENV and VERSION from env vars

--- website/fe/astro.config.mjs
+++ website/fe/astro.config.mjs
@@ -4,8 +4,11 @@ import compress from 'astro-compress'
 import config from '../../config.json'
 
 // environment aware configuration
-const ENV = process.env.ENV || 'dev'
+const ENV = process.env.ENV || 'stg'
+const VERSION = process.env.VERSION || 'dev'
 const domain = config[ENV]?.domain?.name || config.default.domain.name
+const apiDomain = `api.${domain}`
+const apiUrl = `https://${apiDomain}`
 
 const robotsConfig =
   ENV === 'prd'
@@ -40,6 +43,8 @@ export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
   },
   custom: {
     env: ENV,
+    version: VERSION,
+    apiUrl: apiUrl,
   },
   site: import.meta.env.DEV ? 'http://localhost:4321' : `https://${domain}`,
   integrations: [
